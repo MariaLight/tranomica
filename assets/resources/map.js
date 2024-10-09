6ca945d610cb24e0307022abb8dd30a4854307e1
MariaLight <61028743+MariaLight@users.noreply.github.com>
2024-10-09 18:21:15 +1200

    add article and logistics pages

---
+++ assets/resources/map.js
@@ -0,0 +1,25 @@
+let circles = document.querySelectorAll('.circle__hover');
+let popups = document.querySelectorAll('.logistics__popup');
+let map = document.querySelector('.logistics__map--svg');
+
+function closePopups() {
+    popups.forEach(function (item) {
+        item.classList.remove('active')
+    })
+}
+
+map.addEventListener('click', function (event) {
+    console.log(event.target);
+    if (!event.target.parentNode.parentNode.classList.contains('circle__hover') && !event.target.parentNode.classList.contains('circle__hover') && !event.target.classList.contains('circle__hover')) {
+        closePopups();
+    }
+})
+
+circles.forEach(function (item) {
+    item.addEventListener('click', function () {
+        closePopups();
+        let popupId = item.getAttribute("data-popup");
+        document.getElementById(popupId).classList.add('active');
+    })
+})
+
